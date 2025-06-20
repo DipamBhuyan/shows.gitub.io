470f5186d2886936a9489850a2b12d66507344a7
Dipam Bhuyan <81673711+DipamBhuyan@users.noreply.github.com>
2025-06-20 12:50:32 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
 
-    $dbh = new PDO('mysql:host=localhost;dbname=show_record', 'root', 'password');
+    $dbh = new PDO('mysql:host=sql105.infinityfree.com;dbname=if0_39277762_show_record', 'if0_39277762', 'tau0ROCdr3E');
     
     $stmt = $dbh->prepare("Delete from shows where idshows = $_POST[bid]");
 	$stmt->execute();
