e3ee20e756553485c3befb42fc2d354df8dc7843
Dipam Bhuyan <81673711+DipamBhuyan@users.noreply.github.com>
2025-06-20 12:51:36 +0530

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['rid'])){
   if(isset($_POST['name'])){
-      $dbh = new PDO('mysql:host=localhost;dbname=show_record', 'root', 'password');
+      $dbh = new PDO('mysql:host=sql105.infinityfree.com;dbname=if0_39277762_show_record', 'if0_39277762', 'tau0ROCdr3E');
       $stmt = $dbh->prepare("Insert into shows (name, category, completeness, date) values ('$_POST[name]','$_POST[category]','$_POST[completeness]','$_POST[date]')");
       $stmt->execute();
           header("location: record.php");
